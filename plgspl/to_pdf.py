48cfab5efcfbc70f6ab2d5cee36c2f1cbaba549f
m <mbe_pro@outlook.com>
2020-05-27 19:46:57 -0700

    basic parsing debuggin

--- plgspl/to_pdf.py
+++ plgspl/to_pdf.py
@@ -11,24 +11,27 @@ def to_pdf(out_file, info_json, manual_csv, file_dir=None):
     config = qs.AssignmentConfig()
 
     # load the raw assignment config file
+    print(f'Loading config from {info_json}...')
     zones = json.load(open(info_json))['zones']
     for z in zones:
         for i, raw_q in enumerate(z['questions']):
             q = None
             if 'id' not in raw_q:
-                vs = raw_q['alternatives']
+                vs = list(map(lambda q: q['id'], raw_q['alternatives']))
                 q = qs.QuestionInfo(vs[0], i, vs, False, raw_q['numberChoose'])
             else:
                 q = qs.QuestionInfo(raw_q['id'], i)
             config.add_question(q)
-        print(
-            f'Parsed config... Created {config.get_question_count()} questions and {config.get_variant_count()} variants...')
+    print(
+        f'Parsed config. Created {config.get_question_count()} questions and {config.get_variant_count()} variants.', end='\n\n')
 
     # iterate over the rows of the csv and parse the data
-    manual = pd.concat(pd.read_csv(manual_csv))
-    for m in manual.iterrows():
-        uid = m['UIN']
-        qid = m['QID']
+    print(
+        f'Parsing submissions from {manual_csv} and provided file directory (if any)')
+    manual = pd.read_csv(manual_csv)
+    for i, m in manual.iterrows():
+        uid = str(m['UIN'])
+        qid = m['qid']
         sid = m['submission_id']
         submission = submissions.get(uid)
         if not submission:
@@ -36,27 +39,27 @@ def to_pdf(out_file, info_json, manual_csv, file_dir=None):
             submissions[uid] = submission
         q = config.get_question(qid)
 
-        csv = qs.StudentCSV(
-            q, m['params'], m['true_answer'], m['submitted_answer'])
+        csv = qs.StudentCSV(m['params'], m['true_answer'],
+                            m['submitted_answer'])
 
         # look for any files related to this question submission
         if file_dir:
             fns = []
             for fn in os.listdir(file_dir):
                 if fn.find(f'{uid}_{qid}_{sid}'):
-                    fn.append(os.path.join(file_dir, fn))
+                    fns.append(os.path.join(file_dir, fn))
             if len(fns):
                 q.expect_files = True
             file_bundle = qs.StudentFileBundle(fns)
-            sq = qs.StudentQuestion(q, csv, file_bundle,
-                                    qid, int(m['old_score']))
+        submission.add_student_question(
+            qs.StudentQuestion(q, csv, file_bundle, qid, int(m[3])))
+    print(f'Created {len(submissions)} submission(s)..')
 
-    json.dump(submissions, 'tst.json')
-    # pdf = FPDF()
-    # for k, v in submissions:
-    #     v.render_submission(pdf, config)
-    # pdf.output(os.path.join(os.getcwd(), f'{out_file}_submissions.pdf'))
+    pdf = FPDF()
+    for k, v in submissions.items():
+        v.render_submission(pdf, config)
+    pdf.output(os.path.join(os.getcwd(), f'{out_file}_submissions.pdf'))
 
-    # sample_pdf = FPDF()
-    # qs.Submission('SAMPLE').render_submission(pdf, config)
-    # pdf.output(os.path.join(os.getcwd(), f'{out_file}_sample.pdf'))
+    sample_pdf = FPDF()
+    qs.Submission('SAMPLE').render_submission(pdf, config)
+    pdf.output(os.path.join(os.getcwd(), f'{out_file}_sample.pdf'))
