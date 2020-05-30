86f3d676d7a21d9e47f288232bf8513c21948a23
UBC Student <bevansm@students.cs.ubc.ca>
2020-05-29 18:47:40 -0700

    fixing up file append issues and basic stress test on exam 3 data (#3)
    * Fixes file issues
    * Adds the correctness box

--- plgspl/to_pdf.py
+++ plgspl/to_pdf.py
@@ -6,22 +6,24 @@ import os
 import json
 
 
-def to_pdf(out_file, info_json, manual_csv, file_dir=None):
+def to_pdf(info_json, manual_csv, file_dir=None):
     submissions = dict()
     config = qs.AssignmentConfig()
 
     # load the raw assignment config file
-    print(f'Loading config from {info_json}...')
-    zones = json.load(open(info_json))['zones']
+    cfg = json.load(open(info_json))
+    out_file = cfg.get("title", "assignment")
+    zones = cfg['zones']
+    print(f'Parsing config for {out_file}...')
     for z in zones:
         for i, raw_q in enumerate(z['questions']):
             q = None
             if 'id' not in raw_q:
                 vs = list(map(lambda q: q['id'], raw_q['alternatives']))
                 q = qs.QuestionInfo(
-                    vs[0], i, vs, number_choose=raw_q['numberChoose'])
+                    vs[0], i, variants=vs, number_choose=raw_q['numberChoose'])
             else:
-                q = qs.QuestionInfo(raw_q['id'], i)
+                q = qs.QuestionInfo(raw_q['id'], i + 1)
             config.add_question(q)
     print(
         f'Parsed config. Created {config.get_question_count()} questions and {config.get_variant_count()} variants.', end='\n\n')
@@ -39,20 +41,20 @@ def to_pdf(out_file, info_json, manual_csv, file_dir=None):
             submission = qs.Submission(uid)
             submissions[uid] = submission
         q = config.get_question(qid)
+        if not q:
+            continue
 
         # look for any files related to this question submission
+        fns = []
         if file_dir:
-            fns = []
             for fn in os.listdir(file_dir):
-                if fn.find(f'{uid}_{qid}_{sid}'):
+                if fn.find(f'{uid}_{qid}_{sid}') > -1:
                     fns.append(os.path.join(file_dir, fn))
-                    q.add_file(fn)
-            file_bundle = qs.StudentFileBundle(fns, qid)
-
+                    q.add_file(os.path.join(file_dir, fn))
         submission.add_student_question(
             qs.StudentQuestion(q,
                                m['params'], m['true_answer'], m['submitted_answer'],
-                               file_bundle, qid, int(m[3])))
+                               qs.StudentFileBundle(fns, qid), qid, int(m[3])))
     print(f'Created {len(submissions)} submission(s)..')
 
     pdf = PDF()
